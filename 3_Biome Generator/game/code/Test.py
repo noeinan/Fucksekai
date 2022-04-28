07d79ca090327aea0e6573a97b06224535de43d1
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2022-04-28 17:21:42 +0800

    Done

--- 3_Biome Generator/game/code/Test.py
+++ 3_Biome Generator/game/code/Test.py
@@ -4,20 +4,11 @@ Created on Wed Apr 27 23:58:55 2022
 
 @author: ASUS
 """
-import os, sys, random
-env_tiles = [[],[],[],[]]
-base_path = "\\Documents\\GitHub\\FuckSekai\\3_Biome Generator\\game\\gui\\button\\environment_screen\\"
-path = sys.path[0] + base_path
-for files in os.listdir(path):
-    files = files.replace("_hover.png", "")
-    files = files.replace("_idle.png", "")
-    if "north" in files and files not in env_tiles[0]:
-        env_tiles[0].append(files)
-    elif "east" in files and files not in env_tiles[1]:
-        env_tiles[1].append(files)
-    elif "south" in files and files not in env_tiles[2]:
-        env_tiles[2].append(files)
-    elif "west" in files and files not in env_tiles[3]:
-        env_tiles[3].append(files)
-
-print(random.choice(env_tiles[3]))
+for l in range(0, 7):
+    y = (1 / 7) * l
+    y1 = y
+    for n in range(0, 8):
+        x = (1 / 8) * n
+        x1 = x
+        for i in range (0, 4):
+            print(x, y)
